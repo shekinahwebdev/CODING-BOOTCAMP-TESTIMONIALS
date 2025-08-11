791afbe02c4c730d4a24460fbb04e18d6f3690ba
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-08-11 17:39:17 +0000

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import "./App.css";
 
 function App() {
-  return <main className="main_app"></main>;
+  return (
+    <main className="main_app">
+      <div className="image_slider">
+        <div className="slider">
+          <div className="slider_item">
+            <img src="/images/slider1.jpg" alt="Slider 1" />
+          </div>
+          <div className="slider_item">
+            <img src="/images/slider2.jpg" alt="Slider 2" />
+          </div>
+          <div className="slider_item">
+            <img src="/images/slider3.jpg" alt="Slider 3" />
+          </div>
+        </div>
+      </div>
+
+      <div className="testimonial_section">
+        <p className="testimonial_text">
+          "This coding bootcamp has transformed my career! The instructors are
+        </p>
+      </div>
+    </main>
+  );
 }
 
 export default App;
