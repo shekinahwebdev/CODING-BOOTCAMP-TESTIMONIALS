083b4d80a71c7a3d6c7e89d504dd59837fe3d4ee
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-08-17 21:15:50 +0000

    Refactor CSS styles for testimonials and update user info structure

--- src/data.ts
+++ src/data.ts
@@ -9,14 +9,14 @@ export const userInfo = [
     testimonial:
       " I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future. ",
   },
-  {
-    img: {
-      src: "../public/images/image-john.jpg",
-      alt: "image of John Tarkpor",
-    },
-    name: "John Tarkpor",
-    role: "Junior Front-end Developer",
-    testimonial:
-      "If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
-  },
+  // {
+  //   img: {
+  //     src: "../public/images/image-john.jpg",
+  //     alt: "image of John Tarkpor",
+  //   },
+  //   name: "John Tarkpor",
+  //   role: "Junior Front-end Developer",
+  //   testimonial:
+  //     "If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
+  // },
 ];
